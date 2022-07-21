112269faf07d1e032d4d27c610c7535ed24fad85
Raoni87 <raonioliva@gmail.com>
2022-07-20 22:45:59 -0300

    resolução de exercícios

---
+++ exercicio1/script.js
@@ -0,0 +1,8 @@
+let numeroPar = prompt("Insira um número par");
+let resultadoDivisao = numeroPar / 2;
+
+console.log(Number(resultadoDivisao % 2));
+
+//c) Sim. O resto dessa divisão é sempre zero.
+
+//d) Quando usado o número impar, o código retorna um número com resto maior do que zero.
